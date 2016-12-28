044edd3d50a13b46e09ebaeb44bd7de083b86645
bwilson <bwilson@bridge2solutions.com>
2016-12-28 00:03:28 -0500

    I fight for the user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+import { AppComponent, DialogContent} from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogContent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
+  entryComponents: [DialogContent],
   providers: [],
   bootstrap: [AppComponent]
 })
