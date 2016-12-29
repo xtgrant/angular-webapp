a9be5424221d72e57cc61369ac9de92fe9923c4e
bwilson <bwilson@bridge2solutions.com>
2016-12-29 02:20:28 -0500

    added initial routing with home and lost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,48 +9,9 @@ import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
 export class AppComponent {
   title = 'Angular Webapp';
   isDarkTheme: boolean = false;
-  lastDialogResult: string;
 
-  foods: any[] = [
-    {name: 'Pizza', rating: 'Excellent'},
-    {name: 'Burritos', rating: 'Great'},
-    {name: 'French fries', rating: 'Pretty good'},
-  ];
+  constructor() { }
 
-  progress: number = 0;
-
-  constructor(private _dialog: MdDialog, private _snackbar: MdSnackBar) {
-    // Update the value for the progress-bar on an interval.
-    setInterval(() => {
-      this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100;
-    }, 200);
+  ngOnInit() {
   }
-
-  openDialog() {
-    let dialogRef = this._dialog.open(DialogContent);
-
-    dialogRef.afterClosed().subscribe(result => {
-      this.lastDialogResult = result;
-    })
-  }
-
-  showSnackbar() {
-    this._snackbar.open('YUM SNACKS', 'CHEW');
-  }
-}
-
-@Component({
-  template: `
-    <p>This is a dialog</p>
-    <p>
-      <label>
-        This is a text box inside of a dialog.
-        <input #dialogInput>
-      </label>
-    </p>
-    <p> <button md-button (click)="dialogRef.close(dialogInput.value)">CLOSE</button> </p>
-  `,
-})
-export class DialogContent {
-  constructor(@Optional() public dialogRef: MdDialogRef<DialogContent>) { }
 }
